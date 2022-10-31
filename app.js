ebea84b27eb41630888b386d486e096b29dfa7b1
Taha <tahatsahin@gmail.com>
2022-10-31 13:32:24 +0300

    add static files

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-const fn = function () {
-	console.log('test');
-};
+import express from 'express'; // use import instead of require
+import path from 'path';
+
+const app = express();
+
+const PORT = 8080;
+
+// middlewares
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
+
+app.listen(PORT, () => {
+	console.log(`start server on port ${PORT}`);
+});
